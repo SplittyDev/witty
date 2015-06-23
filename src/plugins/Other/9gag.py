5ee160f3ba909b84d7fb6d9c115e9ed948ea6d9d
Splitty <splittydev@gmail.com>
2015-06-23 14:58:22 +0200

    Added 9gag plugin

--- src/plugins/Other/9gag.py
+++ src/plugins/Other/9gag.py
@@ -9,15 +9,15 @@ from yapsy.IPlugin import IPlugin
 class YoutubePlugin(IPlugin):
     def privmsg(self, user, channel, msg):
         manager = PluginManagerSingleton.get()
-        expr = '.*http(?:s)?://(?:w{3}\.)?(?:youtube\.com|youtu\.be)/watch\?v=(?P<id>[0-9a-z_]{11}).*'
+        expr = '.*http(?:s)?://(?:w{3}\.)?9gag.com/gag/(?P<id>[0-9a-zA-Z_]*).*'
         if re.match(expr, msg, re.IGNORECASE):
-            video_id = re.match(expr, msg, re.IGNORECASE).group('id')
-            url = 'https://www.youtube.com/watch/?v=%s' % video_id
+            picture_id = re.match(expr, msg, re.IGNORECASE).group('id')
+            url = 'http://9gag.com/gag/%s' % picture_id
             print(url)
             try:
                 html = urlopen(url)
             except URLError:
                 return
             soup = BeautifulSoup(html)
-            title = soup.find('span', {'class': 'watch-title'}).contents[0]
+            title = soup.find('h2', {'class': 'badge-item-title'}).contents[0]
             manager.app.say(channel, 'Title: %s' % str(title).replace('\n', '').strip())
