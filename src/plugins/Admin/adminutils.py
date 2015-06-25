e47752fdc26cad93d586c8f11fb21ad11fd754cd
Splitty <splittydev@gmail.com>
2015-06-25 02:50:50 +0200

    Added op/deop commands to adminutils plugin

--- src/plugins/Admin/adminutils.py
+++ src/plugins/Admin/adminutils.py
@@ -15,10 +15,33 @@ class AdminUtilsPlugin(IPlugin):
         super(AdminUtilsPlugin, self).__init__()
 
     def privmsg(self, user, channel, msg):
-        app = PluginManagerSingleton.get().app
+        manager = PluginManagerSingleton.get()
+        app = manager.app
         authenticated = True
         if user not in self.config['admins']:
             authenticated = False
+        if msg == '_ops':
+            if not self.config['admins']:
+                app.say(channel, 'No administrators found.')
+            else:
+                for operator in self.config['admins']:
+                    msg = 'Operators: %s' % ', '.join(operator)
+                    app.say(channel, msg)
+        elif msg.startswith('_op'):
+            _user = msg[3:].strip()
+            if _user not in self.config['admins']:
+                self.config['admins'].append(_user)
+                self.manager.wittyconf.update_plugin_config(self.plugin_name, self.config)
+                app.say(channel, 'Gave %s permission to use adminutils.' % _user)
+        if msg.startswith('_deop'):
+            _user = msg[3:].strip()
+            removed = 0
+            while _user in self.config['admins']:
+                self.config['admins'].remove(_user)
+                removed += 1
+            if removed > 0:
+                app.say(channel, 'Took admin permissions from %s' % _user)
+                self.manager.wittyconf.update_plugin_config(self.plugin_name, self.config)
         if msg == '_quit':
             if authenticated:
                 app.quit('bye')
